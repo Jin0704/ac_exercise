a7fd6540694a1a2d3d8aa1bf9fbab8f95d60a1c8
Jin <s87160204@gmail.com>
2020-07-25 16:53:11 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -3,11 +3,29 @@
 const express = require('express')
 const app = express()
 
+const mongoose = require('mongoose') // 載入 mongoose
+
+mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
+
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 若連線異常
+db.on('error', () => {
+  console.log('mongodb error')
+})
+
+//連線成功
+db.once('open', () => {
+  console.log('mongodb connected')
+})
+
 // 設定首頁路由
 app.get('/', (req, res) => {
   res.send('Hello~~~')
-
 })
+
+
 
 // 設定 port 3000
 app.listen(3000, () => {
